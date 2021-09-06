865c4d5946a3f14af330aea9974819401433461f
Lars Ole Algrøy <Lars_ole_algroy@outlook.com>
2021-09-06 16:01:03 +0200

    shouldGetRespons(fail)

---
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -0,0 +1,10 @@
+package no.kristiania.http;
+
+public class HttpClient {
+    public HttpClient(String s, int i, String s1) {
+    }
+
+    public int getStatuscode() {
+        return 0;
+    }
+}
